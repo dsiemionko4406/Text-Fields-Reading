59c2e61a567a7a9d8e41607cece7a8e0a5389514
David Siemionko <dsiemionko4406@stu.d214.org>
2020-10-26 14:39:23 -0500

    Initial Commit

---
+++ Text Fields Reading/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Text Fields Reading
+//
+//  Created by David Siemionko on 10/26/20.
+//  Copyright © 2020 David Siemionko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
